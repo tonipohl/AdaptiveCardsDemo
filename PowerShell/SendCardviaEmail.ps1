7d407d508b12042e6edc8b7a58536230efb170ff
Toni Pohl <Toni.Pohl@atwork.at>
2019-05-28 15:23:42 +0200

    init

--- PowerShell/SendCardviaEmail.ps1
+++ PowerShell/SendCardviaEmail.ps1
@@ -1,4 +1,4 @@
-﻿$cardJson = Get-Content .\expenseCard.json
+﻿$cardJson = Get-Content C:\Git\AdaptiveCardsDemo\PowerShell\expenseCard.json
 $cardPayload = "<script type='application/ld+json'>" + $cardJson + "</script>"
 
 $htmlMessage = "<html><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8'>"
@@ -7,7 +7,8 @@ $htmlMessage += "</head><body>"
 $htmlMessage += "Visit the <a href='https://docs.microsoft.com/en-us/outlook/actionable-messages'>Outlook Dev Portal</a> to learn more about Actionable Messages."
 $htmlMessage += "</body></html>"
 
-$msolcred = Get-Credential
+#$msolcred = Get-Credential
+
 Send-MailMessage –From $msolcred.UserName –To $msolcred.UserName `
     -Subject "MessageCard Demo" `
     -Body $htmlMessage -BodyAsHtml `
